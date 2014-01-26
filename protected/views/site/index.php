04d9683e23c7fe7095b6cd3bdf3b4ee7fb7f6a23
asrojas <guitmet@gmail.com>
2014-01-26 17:19:35 -0300

    cambio vista 1.3

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -10,33 +10,68 @@
         <!-- #################### cuerpo  #######################--> 
 
 
-        <h3><u> Bienvenido <?php echo $usuario->nom_usuario;  ?></u> </h3>    
+           
 
-        <!-- #####################  toma de turnos  ###################--> 
-        <div class="well">
-            <h3>Toma de turnos: Jueves - 23:00 hrs</h3>
-            
-          <!-- ############## reloj ################3-->
+        <!-- #####################  noticias  ###################--> 
+        <div class="    ">
         
-          <?php // echo date('w'); ?>
+            <div class="">
+        <div class="row-fluid" style="background-color: #00b2d9; color:white;">
+            <div class="span12">
+               <h3 style="margin-left: 20px;"> Noticias</h3> 
+            </div>    
+        </div>
+        </div>
             
-<!--          <script>
             
-                var V = "<?php echo date('w'); ?>";
             
-                alert(V);
-          </script>  -->
-          
-          <!-- ############# fin del reloj ############### -->
+            <div class="bs-docs-example">
+                <div class="alert alert-block alert-error fade in">
+                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                    <strong>Anuncio!</strong> Nueva plataforma de Servicios de Empaques Universitarios.
+                </div>
+            </div>
+             
+            <div class="bs-docs-example">
+                <div class="alert alert-block alert-error fade in">
+                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                    <strong>Publicaciones!</strong> Podrán ver sus faltas respectivas.
+                </div>
+            </div>
+            
+             <div class="bs-docs-example">
+                <div class="alert alert-block alert-error fade in">
+                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                    <strong>Anuncio!</strong> Acuérdese de leer constantemente el reglamento.
+                </div>
+            </div>
+      
         </div>    
             
-        <h3><u>Turnos de la semana</u></h3>
-
-         
-            
+        <div class="container-fluid">
+        <div class="row-fluid">
+            <div class="span12">
+              
+                <div class="border_ini">
+                    
+                </div>
+                
+            </div>
+        </div>
+        </div>
+        
+      
        <!-- #############   faltas que tiene el usuario  ################-->
             
-        <h3><u>Últimas faltas </u></h3>
+       <div class="">
+        <div class="row-fluid" style="background-color: #00b2d9; color:white;">
+            <div class="span12">
+                <h3 style="margin-left: 20px;">Últimas faltas </h3>
+            </div>    
+        </div>
+        </div>
+       
+        
 
             <?php
                 
@@ -52,22 +87,152 @@
       
                 ?>
 
-                    <div class="well">
-    
-                         <?php echo "<b>Nombre:</b> ".$f->desc_falta."<br>"; ?>
-                         <?php echo "<b>Tipo: </b>".$f->tipo_falta."<br>"; ?>
-                         <?php echo "<b>Fecha: </b>".$f->fecha_falta."<br>"; ?>
-                         
+                    <div class="mis_turnos">
+                        <div class="row-fluid">
+                            <div class="">
+                            <?php echo "<div class='span4'><b>Descripción:</b> ".$f->desc_falta."</div>"; ?>
+                            <?php echo "<div class='span2'><b>Tipo: </b>".$f->tipo_falta."</div>"; ?>
+                            <?php echo "<div class='span2'><b>Fecha: </b>".$f->fecha_falta."</div>"; ?>
+
+                            </div>
+                        </div>    
                     </div>  
 
 
                 <?php } ?>
 
 
+     <div class="container-fluid">
+        <div class="row-fluid">
+            <div class="span12">
+              
+                <div class="border_ini">
+                    
+                </div>
+                
+            </div>
+        </div>
+        </div>   
+        
+        <!-- ##############  turnos de la semana ############## -->
+        <div class="">
+        <div class="row-fluid" style="background-color: #00b2d9; color:white;">
+            <div class="span12">
+                <h3 style="margin-left: 20px;">Turnos de la semana</h3>
+            </div>    
+        </div>
+        </div>
+     
+<?php $empaque=Usuarios::model()->findByPk(Yii::app()->session['var']);  ?>
 
+
+<?php
+    $turno1 = UsuariosHasTurnos::model()->findAllByAttributes(array('usuarios_id_usuario'=>Yii::app()->session['var']));
+    $i=1;
+    
+    $criteria = new CDbCriteria();
+    $criteria->order = "fecha_turno ASC";
+    
+    if($turno1 != null)
+    {
+        foreach($turno1 as $t)
+    {
+        ?>
+        <div class="row-fluid">
+          
+        <div class=" alinear">
+          <div class="mis_turnos">    
+        <?php
+        $turno2 = Turnos::model()->findByAttributes(array('id_turno'=>$t->turnos_id_turno));
+//        $turno2 = $turno_s->$criteria;
+//        $turno2->find($criteria);
+        
+        $model3 = Yii::app()->db->createCommand("select estado from usuarios_has_turnos
+                where usuarios_id_usuario=".Yii::app()->session['var']." and estado='Aceptado' and turnos_id_turno=".$turno2->id_turno.";")->queryScalar();
+        $model4 = Yii::app()->db->createCommand("select usuarios_id_usuario from usuarios_has_turnos
+                where estado='Rechazado' and turnos_id_turno=".$turno2->id_turno.";")->queryScalar();
+        
+//        echo $turno2->fecha_turno;
+        
+        $t3 = explode(" ", $turno2->fecha_turno);
+        $date1 = date_create($turno2->fecha_turno);
+
+        echo "<div class='span3'> <b>Fecha:</b> "; 
+
+        if(date_format($date1, "w") == "1") echo "Lunes";
+        if(date_format($date1, "w") == "2") echo "Martes";
+        if(date_format($date1, "w") == "3") echo "Miercoles";
+        if(date_format($date1, "w") == "4") echo "Jueves";
+        if(date_format($date1, "w") == "5") echo "Viernes";
+        if(date_format($date1, "w") == "6") echo "Sabado";
+        if(date_format($date1, "w") == "0") echo "Domingo";
+
+        echo " ".$t3[0]."</div> <div class='span3'><b>Hora:</b> ".$t3[1]."</div>";
+        
+        if($model3 == "Aceptado")
+        {
+            echo "<div class='span3'><b>Estado:</b> Aceptado </div>";
+
+            if($model4)
+            {
+                $model5 = Usuarios::model()->find('id_usuario='.$model4);
+                echo "<div class='well'><b>Regalado por ".$model5->nom_usuario." ".$model5->apel1_usuario." ".$model5->apel2_usuario."</b></div>";
+            }
+            
+            
+            
+
+            echo "<div class='span3'><div type=\"button\" class=\"btn btn-mini btn-warning\" onclick=\"location.href='".Yii::app()->baseUrl."/site/regalaturno/".$turno2->id_turno."';\">Regalar turno</div> </div>";
+        }
+        else
+        {
+            echo "<div class='span3'><b>Estado:</b> Regalado </div>";
+            echo "<br>";
+        }
+        
+        $i++;
+        ?>
+        </div>
+        </div>
+        </div>
+        <?php
+                
+    }
+        
+    }
+    else
+    {
+        echo "No se encuentran turnos registrados en esta semana";
+    }
+
+?>     
+        
+        
+    <div class="container-fluid">
+        <div class="row-fluid">
+            <div class="span12">
+              
+                <div class="border_ini">
+                    
+                </div>
+                
+            </div>
+        </div>
+        </div>   
+    
+    
    <!-- ##############  turnos para tomar ##################### -->
-        <h3><u>Turnos para tomar</u></h3>
-
+        
+   <div class="">
+        <div class="row-fluid" style="background-color: #00b2d9; color:white;">
+            <div class="span12">
+                  <h3 style="margin-left: 20px;">Turnos para tomar</h3>
+            </div>    
+        </div>
+        </div>
+   
+          
+   <h1> En construcción </h1>
         
         <!-- #################### fin cuerpo ################### -->
         
