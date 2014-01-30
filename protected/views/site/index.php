952dbf16c10c29d6d08f2bf4bbc9ddd6f838fd79
Anibal Bastias Soto <anibal_bastias@hotmail.com>
2014-01-30 18:15:27 -0300

    Fix v6 index.php

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -504,3 +504,13 @@
 <?php
             }
 ?>
+
+<script type="text/javascript">
+var _urq = _urq || [];
+_urq.push(['initSite', '3bc3377b-dd13-4004-92f2-3ec6788e9dcd']);
+(function() {
+var ur = document.createElement('script'); ur.type = 'text/javascript'; ur.async = true;
+ur.src = ('https:' == document.location.protocol ? 'https://cdn.userreport.com/userreport.js' : 'http://cdn.userreport.com/userreport.js');
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ur, s);
+})();
+</script>
